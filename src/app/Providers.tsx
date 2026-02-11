171ab9388e102bfc4b85995861392efa27c9b684
Karl Musingo <karlemusingo@gmail.com>
2026-02-11 20:19:52 +0200

    fix: fix NEXT_PUBLIC_CONVEX_URL

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -5,7 +5,7 @@ import { ConvexReactClient } from "convex/react";
 import { ConvexAuthProvider } from "@convex-dev/auth/react";
 
 const convex = new ConvexReactClient(
-  process.env.NEXT_PUBLIC_CONVEX_URL as string
+  process.env.NEXT_PUBLIC_CONVEX_URL as string,
 );
 
 const Providers: FC<PropsWithChildren> = ({ children }) => {
