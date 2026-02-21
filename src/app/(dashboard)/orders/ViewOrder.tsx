6e6841b1df80ca0a4edebebdd683dd982da8c294
Karl Musingo <karlemusingo@gmail.com>
2026-02-21 22:16:51 +0200

    chore(order): add time to orders and make it french formatted

--- src/app/(dashboard)/orders/ViewOrder.tsx
+++ src/app/(dashboard)/orders/ViewOrder.tsx
@@ -142,7 +142,7 @@ const ViewOrder: FC<ViewOrderProps> = ({
                   Date:{" "}
                   {new Date(
                     orderData?._creationTime || orderData?.createdAt || new Date()
-                  ).toLocaleDateString()}
+                  ).toLocaleString("fr-FR", { day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: false })}
                 </p>
               </div>
             </CardHeader>
--- src/app/(dashboard)/orders/table.tsx
+++ src/app/(dashboard)/orders/table.tsx
@@ -84,7 +84,7 @@ export const getColumns = ({ onEdit, onView, profile }: ActionsFuncType) => {
       cell: ({ row }) => {
         const rowData = row.original;
 
-        return new Date(rowData._creationTime || rowData.createdAt).toLocaleDateString();
+        return new Date(rowData._creationTime || rowData.createdAt).toLocaleString("fr-FR", { day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: false });
       },
     },
 
